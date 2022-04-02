18f63eb7fcb47a342886b8c45393050a8842ad0c
Bryan Sanchez <bryanipsanchez@gmail.com>
2022-04-02 20:58:19 +0800

    Imported JSON data

--- client/src/App.js
+++ client/src/App.js
@@ -3,8 +3,10 @@ import Comments from './components/Comments/Comments'
 import Form from './components/Form/Form'
 import Footer from './components/Footer/Footer'
 import Modal from './components/Modal/Modal'
+import data from './data.json'
 
 function App() {
+  console.log(data);
 
   return (
     <div className="app">
