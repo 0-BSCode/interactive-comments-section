64748039761562b2bfa7e93b513b17a6a8958f12
Bryan Sanchez <bryanipsanchez@gmail.com>
2022-04-02 14:16:43 +0800

    Compartmentalized Footer

--- client/src/App.js
+++ client/src/App.js
@@ -1,148 +1,17 @@
 import './App.css';
 import Comments from './components/Comments/Comments'
 import Form from './components/Form/Form'
+import Footer from './components/Footer/Footer'
 
 function App() {
 
   return (
     <div className="App">
-      <Comments />
-      <Form />
-      {/* <section>
-        <article>
-          <header>
-            <img src={profileImg} />
-            <h3>
-              amyrobson
-            </h3>
-            <p>
-              1 month ago
-            </p>
-          </header>
-          <p>
-          Impressive! Though it seems the drag feature could be improved. But overall it looks incredible. You've nailed the design and the responsiveness at various breakpoints works really well.
-          </p>
-          <div>
-            <span>
-              <button>
-                <img src={addImg} />
-              </button>
-              <p>12</p>
-              <button>
-                <img src={subtractImg} />
-              </button>
-            </span>
-            <button>
-              <img src={replyImg} />
-              <p>Reply</p>
-            </button>
-          </div>
-        </article>  
-      </section>
-      <section>
-        <article>
-          <header>
-            <img src={profileImg} />
-            <h3>
-              amyrobson
-            </h3>
-            <p>
-              1 month ago
-            </p>
-          </header>
-          <p>
-          Impressive! Though it seems the drag feature could be improved. But overall it looks incredible. You've nailed the design and the responsiveness at various breakpoints works really well.
-          </p>
-          <div>
-            <span>
-              <button>
-                <img src={addImg} />
-              </button>
-              <p>12</p>
-              <button>
-                <img src={subtractImg} />
-              </button>
-            </span>
-            <button>
-              <img src={replyImg} />
-              <p>Reply</p>
-            </button>
-          </div>
-        </article>  
-      </section>
-      <section>
-        <article>
-          <header>
-            <img src={profileImg} />
-            <h3>
-              amyrobson
-            </h3>
-            <p>
-              1 month ago
-            </p>
-          </header>
-          <p>
-          Impressive! Though it seems the drag feature could be improved. But overall it looks incredible. You've nailed the design and the responsiveness at various breakpoints works really well.
-          </p>
-          <div>
-            <span>
-              <button>
-                <img src={addImg} />
-              </button>
-              <p>12</p>
-              <button>
-                <img src={subtractImg} />
-              </button>
-            </span>
-            <button>
-              <img src={replyImg} />
-              <p>Reply</p>
-            </button>
-          </div>
-        </article>
-        <div>
-          <section>
-            <article>
-              <header>
-                <img src={profileImg} />
-                <h3>
-                  amyrobson
-                </h3>
-                <p>
-                  1 month ago
-                </p>
-              </header>
-              <p>
-              Impressive! Though it seems the drag feature could be improved. But overall it looks incredible. You've nailed the design and the responsiveness at various breakpoints works really well.
-              </p>
-              <div>
-                <span>
-                  <button>
-                    <img src={addImg} />
-                  </button>
-                  <p>12</p>
-                  <button>
-                    <img src={subtractImg} />
-                  </button>
-                </span>
-                <button>
-                  <img src={replyImg} />
-                  <p>Reply</p>
-                </button>
-              </div>
-            </article>  
-          </section>
-        </div>
-      </section>
-      <form>
-        <input type="text" />
-        <div>
-          <img src={profileImg} />
-          <button type="submit">
-            Send
-          </button>
-        </div>
-      </form> */}
+      <main className="App__main">
+        <Comments />
+        <Form />
+      </main>
+      <Footer />
     </div>
   );
 }
---
+++ client/src/components/Footer/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className="attribution">
+        <p>
+            Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
+            Coded by <a href="#">Your Name Here</a>.
+        </p>
+    </footer>
+  )
+}
+
+export default Footer
