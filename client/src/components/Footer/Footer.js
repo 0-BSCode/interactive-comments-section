24136cfa5d5d3f6b5540fb941ba9cb65a476503c
Bryan Sanchez <bryanipsanchez@gmail.com>
2022-04-02 20:15:31 +0800

    Added styles

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import './Footer.css'
 
 const Footer = () => {
   return (
     <footer className="attribution">
-        <p>
+        <p className="attribution__text">
             Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
-            Coded by <a href="#">Your Name Here</a>.
+            Coded by <a href="#">BSCoder</a>.
         </p>
     </footer>
   )
