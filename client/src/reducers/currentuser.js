1c879499106c962cb9e540faadc3510c9e5c0731
Bryan Sanchez <bryanipsanchez@gmail.com>
2022-04-06 23:32:12 +0800

    Edited initial state

--- client/src/reducers/currentuser.js
+++ client/src/reducers/currentuser.js
@@ -1,6 +1,6 @@
 import data from '../data.json'
 
-export default (state=[data.currentUser], action) => {
+export default (state=data.currentUser, action) => {
     switch(action.type) {
         default:
             return state;
