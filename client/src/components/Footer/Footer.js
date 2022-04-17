0688437028647c050acddede9b5fd01aeaf7a1a9
Bryan Sanchez <bryanipsanchez@gmail.com>
2022-04-17 16:26:23 +0800

    Added link

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
     <footer className="attribution">
         <p className="attribution__text">
             Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
-            Coded by <a href="#">BSCode</a>.
+            Coded by <a href="https://github.com/0-BSCode">BSCode</a>.
         </p>
     </footer>
   )
